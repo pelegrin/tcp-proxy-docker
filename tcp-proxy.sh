2ddccbfbc32368a721a4ed26b366b3e51d100311
Dmitri Savustjan <pelegrin@users.noreply.github.com>
2019-10-22 15:42:48 +0300

    Use env variables instead of parameters

--- tcp-proxy.sh
+++ tcp-proxy.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 2 ]; then
-    echo "usage: $(basename $0) host port"
-    exit 1
-fi
-
-HOST="$1"
-PORT="$2"
-
 LISTEN_PORT=${LISTEN_PORT:-${PORT}}
 
 echo "relay TCP/IP connections on :${LISTEN_PORT} to ${HOST}:${PORT}"
